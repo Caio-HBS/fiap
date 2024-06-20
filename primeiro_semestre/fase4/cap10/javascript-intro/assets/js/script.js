741153cce57188838d190f226b2cf84528907b87
Caio Bianchi <caiohbs23@gmail.com>
2024-06-20 10:25:53 -0300

    adicionar "javascript-intro"

---
+++ primeiro_semestre/fase4/cap10/javascript-intro/assets/js/script.js
@@ -0,0 +1,11 @@
+// Comentários em Js
+
+/*
+Comentários
+em mais de
+uma linha
+*/
+
+console.log("Olá, eu sou um console!");
+console.log("Olá, eu sou um console e estou usando aspas simples");
+console.log(`Olá, eu sou um console e estou usando o acento grave`);
