2ce708794a59224cbfc118cfd22b246321553683
Caio Bianchi <caiohbs23@gmail.com>
2024-09-24 16:57:53 -0300

    adicionado .sql

---
+++ segundo_semestre/fase6/cap7/comandos.sql
@@ -0,0 +1,21 @@
+-- Cadastro e alteracao
+
+-- Cadastro de dados usuario.
+INSERT INTO T_USUARIO (
+    id_usuario, nome, sobrenome, dt_nascimento, email, nm_usuario, senha
+    ) VALUES (
+        1, 'Jose', 'Amaro', TO_DATE('20/03/1989', 'dd/mm/yyyy'), 'exemplo@email.com', 'jose_amaro', 'senha-segura'
+    )
+
+-- Cadastrar os dados para a conta do usuario.
+
+
+-- Alterar todos os dados do usuario, utilizando seu c�digo como refer�ncia.
+-- Cadastrar as receitas do usuario.
+-- Alterar todos os dados das receitas do usu�rio, utilizando o c�digo como refer�ncia.
+-- Cadastrar as despesas do usuario.
+-- Alterar todos os dados das despesas do usuario, utilizando o codigo como referencia.
+-- Cadastrar os dados para investimentos.
+
+
+-- Alterar todos os dados para investimentos do usu�rio.
