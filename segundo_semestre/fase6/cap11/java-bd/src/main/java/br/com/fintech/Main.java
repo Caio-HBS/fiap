741c96d9f6afa549b5a1bbe93e1c5089289a3c43
Caio Bianchi <caiohbs23@gmail.com>
2024-10-01 17:52:41 -0300

    mudanca em classes

--- segundo_semestre/fase6/cap11/java-bd/src/main/java/br/com/fintech/Main.java
+++ segundo_semestre/fase6/cap11/java-bd/src/main/java/br/com/fintech/Main.java
@@ -1,41 +1,20 @@
 package br.com.fintech;
 
-import br.com.fintech.exception.RecursoNaoEncontradoException;
-import br.com.fintech.model.Usuario;
-import br.com.fintech.model.UsuarioDAO;
-
-import java.sql.SQLException;
-import java.sql.Date;
+import br.com.fintech.exception.TabelaNaoEncontradaException;
+import br.com.fintech.views.InvestimentoView;
 
 public class Main {
 
-    public static void main(String[] args) {
-        //String dateString = "1994-08-25";
+    public static void main(String[] args) throws TabelaNaoEncontradaException {
 
-        //Date date = Date.valueOf(dateString);
-
-        //try {
-        //    UsuarioDAO dao = new UsuarioDAO();
-        //    Usuario usuario = new Usuario("João", "Dias", date, "joao@email.com", "joao-dias", "senha" +
-        //
-        //                                                                                                    "-segura");
-        //    dao.cadastrarUsuario(usuario);
-        //    dao.fecharConexao();
-        //    System.out.println("Usuário cadastrado!");
-        //} catch (SQLException e) {
-        //    System.err.println(e.getMessage());
-        //}
-
-        try {
-            UsuarioDAO dao = new UsuarioDAO();
-            Usuario usuario = dao.pesquisarUsuario(16L);
-            System.out.println(usuario);
-            dao.fecharConexao();
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
-        } catch (RecursoNaoEncontradoException e) {
-            System.out.println(e.getMessage());
+        // Testar investimentos
+        boolean result_inv = InvestimentoView.testarInvestimentos();
+        if (result_inv) {
+            System.out.println("\nRESULTADO: Testes de investimentos bem-sucedidos!");
+        } else {
+            System.out.println("\nRESULTADO: Testes de investimento falharam.");
         }
+
     }
 
 }
--- segundo_semestre/fase6/cap11/java-bd/src/main/java/br/com/fintech/factory/ConnectionFactory.java
+++ segundo_semestre/fase6/cap11/java-bd/src/main/java/br/com/fintech/factory/ConnectionFactory.java
@@ -1,17 +1,33 @@
 package br.com.fintech.factory;
 
+import br.com.fintech.exception.TabelaNaoEncontradaException;
+import br.com.fintech.exception.ValidacaoException;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
 
-    private static final String URL = System.getenv("url");
+    private static final String URL = System.getenv("url"); // Variáveis de ambiente para informações sensíveis
     private static final String USUARIO = System.getenv("usuario");
     private static final String SENHA = System.getenv("senha");
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USUARIO, SENHA);
+    public static Connection getConnection() throws TabelaNaoEncontradaException, ValidacaoException {
+        Connection connection = null;
+        
+        try {
+            connection = DriverManager.getConnection(URL, USUARIO, SENHA);
+        } catch (SQLException e) {
+            if (e.getErrorCode() == 942) {
+                throw new TabelaNaoEncontradaException("TABELA", "tabela não encontrada.");
+            }
+
+            if (e.getErrorCode() == 1017) {
+                throw new ValidacaoException();
+            }
+        }
+        return connection;
     }
 
 }
