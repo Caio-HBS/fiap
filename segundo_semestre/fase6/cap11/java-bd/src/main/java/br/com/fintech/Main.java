a4745f7bb0f6ef366f409417174d84adb91de31c
Caio Bianchi <caiohbs23@gmail.com>
2024-10-06 16:59:51 -0300

    projeto finalizado

--- segundo_semestre/fase6/cap11/java-bd/src/main/java/br/com/fintech/Main.java
+++ segundo_semestre/fase6/cap11/java-bd/src/main/java/br/com/fintech/Main.java
@@ -1,27 +1,20 @@
 package br.com.fintech;
 
-import br.com.fintech.exception.*;
-import br.com.fintech.factory.ConnectionFactory;
-import br.com.fintech.model.ServicoUnico;
-import br.com.fintech.model.ServicoUnicoDAO;
 import br.com.fintech.views.InvestimentoView;
+import br.com.fintech.views.ServicoRecorrenteView;
 import br.com.fintech.views.ServicoUnicoView;
-
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
         // Testar investimentos
-        //boolean result_inv = InvestimentoView.testarInvestimentos();
-        //if (result_inv) {
-        //    System.out.println("\nRESULTADO: Testes de investimentos bem-sucedidos!");
-        //} else {
-        //    System.out.println("\nRESULTADO: Testes de investimento falharam.");
-        //}
+        boolean result_inv = InvestimentoView.testarInvestimentos();
+        if (result_inv) {
+            System.out.println("\nRESULTADO: Testes de investimentos bem-sucedidos!");
+        } else {
+            System.out.println("\nRESULTADO: Testes de investimento falharam.");
+        }
 
         // Testar serviços únicos
         boolean result_su = ServicoUnicoView.testarServicosUnicos();
@@ -29,6 +22,14 @@ public class Main {
             System.out.println("\nRESULTADO: Testes de serviços únicos bem-sucedidos!");
         } else {
             System.out.println("\nRESULTADO: Testes de serviços únicos falharam.");
+        }
+
+        // Testar serviços recorrentes
+        boolean result_sr = ServicoRecorrenteView.testarServicosRecorrentes();
+        if (result_sr) {
+            System.out.println("\nRESULTADO: Testes de serviços recorrentes bem-sucedidos!");
+        } else {
+            System.out.println("\nRESULTADO: Testes de serviços recorrentes falharam.");
         }
 
     }
