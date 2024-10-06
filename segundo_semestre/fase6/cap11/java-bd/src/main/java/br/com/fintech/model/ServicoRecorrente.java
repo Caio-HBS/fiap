05b7c152f79680beb88051171c0dbbf82c38bcd9
Caio Bianchi <caiohbs23@gmail.com>
2024-10-06 16:59:22 -0300

    criado servico recorrente

--- segundo_semestre/fase6/cap11/java-bd/src/main/java/br/com/fintech/model/ServicoRecorrente.java
+++ segundo_semestre/fase6/cap11/java-bd/src/main/java/br/com/fintech/model/ServicoRecorrente.java
@@ -1,4 +1,170 @@
 package br.com.fintech.model;
 
+import java.sql.Date;
+
 public class ServicoRecorrente {
+
+    Long idServRecorrente;
+    Long idUsuario;
+    Long idContaBancaria;
+    String nmServico;
+    String descServico;
+    Date dtRegistroServ;
+    Date dtLimite;
+    String frequencia;
+    double valorOperacao;
+    char operacaoEfetuada;
+    char aPagar;
+    char aReceber;
+
+    public ServicoRecorrente() {
+    }
+
+    public ServicoRecorrente(
+            String nmServico, String descServico, Date dtRegistroServ, Date dtLimite, String frequencia,
+            double valorOperacao, char operacaoEfetuada, char aPagar, char aReceber
+    ) {
+        this.nmServico = nmServico;
+        this.descServico = descServico;
+        this.dtRegistroServ = dtRegistroServ;
+        this.dtLimite = dtLimite;
+        this.frequencia = frequencia;
+        this.valorOperacao = valorOperacao;
+        this.operacaoEfetuada = operacaoEfetuada;
+        this.aPagar = aPagar;
+        this.aReceber = aReceber;
+    }
+
+    public ServicoRecorrente(
+            Long idServRecorrente, Long idUsuario, Long idContaBancaria, String nmServico, String descServico,
+            Date dtRegistroServ, Date dtLimite, String frequencia, double valorOperacao, char operacaoEfetuada,
+            char aPagar, char aReceber
+    ) {
+        this.idServRecorrente = idServRecorrente;
+        this.idUsuario = idUsuario;
+        this.idContaBancaria = idContaBancaria;
+        this.nmServico = nmServico;
+        this.descServico = descServico;
+        this.dtRegistroServ = dtRegistroServ;
+        this.dtLimite = dtLimite;
+        this.frequencia = frequencia;
+        this.valorOperacao = valorOperacao;
+        this.operacaoEfetuada = operacaoEfetuada;
+        this.aPagar = aPagar;
+        this.aReceber = aReceber;
+    }
+
+    public Long getIdServRecorrente() {
+        return idServRecorrente;
+    }
+
+    public void setIdServRecorrente(Long idServRecorrente) {
+        this.idServRecorrente = idServRecorrente;
+    }
+
+    public Long getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Long idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public Long getIdContaBancaria() {
+        return idContaBancaria;
+    }
+
+    public void setIdContaBancaria(Long idContaBancaria) {
+        this.idContaBancaria = idContaBancaria;
+    }
+
+    public String getNmServico() {
+        return nmServico;
+    }
+
+    public void setNmServico(String nmServico) {
+        this.nmServico = nmServico;
+    }
+
+    public String getDescServico() {
+        return descServico;
+    }
+
+    public void setDescServico(String descServico) {
+        this.descServico = descServico;
+    }
+
+    public Date getDtRegistroServ() {
+        return dtRegistroServ;
+    }
+
+    public void setDtRegistroServ(Date dtRegistroServ) {
+        this.dtRegistroServ = dtRegistroServ;
+    }
+
+    public Date getDtLimite() {
+        return dtLimite;
+    }
+
+    public void setDtLimite(Date dtLimite) {
+        this.dtLimite = dtLimite;
+    }
+
+    public String getFrequencia() {
+        return frequencia;
+    }
+
+    public void setFrequencia(String frequencia) {
+        this.frequencia = frequencia;
+    }
+
+    public double getValorOperacao() {
+        return valorOperacao;
+    }
+
+    public void setValorOperacao(double valorOperacao) {
+        this.valorOperacao = valorOperacao;
+    }
+
+    public char getOperacaoEfetuada() {
+        return operacaoEfetuada;
+    }
+
+    public void setOperacaoEfetuada(char operacaoEfetuada) {
+        this.operacaoEfetuada = operacaoEfetuada;
+    }
+
+    public char getaPagar() {
+        return aPagar;
+    }
+
+    public void setaPagar(char aPagar) {
+        this.aPagar = aPagar;
+    }
+
+    public char getaReceber() {
+        return aReceber;
+    }
+
+    public void setaReceber(char aReceber) {
+        this.aReceber = aReceber;
+    }
+
+    @Override
+    public String toString() {
+        return "ServicoRecorrente{" +
+               "idServRecorrente=" + idServRecorrente +
+               ", idUsuario=" + idUsuario +
+               ", idContaBancaria=" + idContaBancaria +
+               ", nmServico='" + nmServico + '\'' +
+               ", descServico='" + descServico + '\'' +
+               ", dtRegistroServ=" + dtRegistroServ +
+               ", dtLimite=" + dtLimite +
+               ", frequencia='" + frequencia + '\'' +
+               ", valorOperacao=" + valorOperacao +
+               ", operacaoEfetuada=" + operacaoEfetuada +
+               ", aPagar=" + aPagar +
+               ", aReceber=" + aReceber +
+               '}';
+    }
 }
--- segundo_semestre/fase6/cap11/java-bd/src/main/java/br/com/fintech/model/ServicoRecorrenteDAO.java
+++ segundo_semestre/fase6/cap11/java-bd/src/main/java/br/com/fintech/model/ServicoRecorrenteDAO.java
@@ -1,4 +1,91 @@
 package br.com.fintech.model;
 
+import br.com.fintech.exception.*;
+import br.com.fintech.factory.ConnectionFactory;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServicoRecorrenteDAO {
+
+    private Connection conexao;
+
+    public ServicoRecorrenteDAO() {
+        try {
+            conexao = ConnectionFactory.getConnection();
+        } catch (TabelaNaoEncontradaException | ValidacaoException | NaoFoiPossivelConectarException |
+                 GenericaException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    public void insert(ServicoRecorrente sr) throws ErroNaQueryException {
+        try {
+            PreparedStatement stm = conexao.prepareStatement(
+                    "INSERT INTO t_serv_recorrente (id_serv_recorrente, id_usuario, id_conta_bancaria, " +
+                    "nm_servico, desc_servico, dt_registro_serv, dt_limite, frequencia, valor_operacao, " +
+                    "operacao_efetuada, a_pagar, a_receber) VALUES (seq_serv_recorrente.nextval, 1, 2, ?, " +
+                    "?, ?, ?, ?, ?, ?, ?, ?)"
+            );
+
+            stm.setString(1, sr.getNmServico());
+            stm.setString(2, sr.getDescServico());
+            stm.setDate(3, sr.getDtRegistroServ());
+            stm.setDate(4, sr.getDtLimite());
+            stm.setString(5, sr.getFrequencia());
+            stm.setDouble(6, sr.getValorOperacao());
+            stm.setString(7, String.valueOf(sr.getOperacaoEfetuada()));
+            stm.setString(8, String.valueOf(sr.getaPagar()));
+            stm.setString(9, String.valueOf(sr.getaReceber()));
+
+            stm.executeUpdate();
+
+
+        } catch (SQLException e) {
+            throw new ErroNaQueryException(e.getMessage());
+        }
+    }
+
+    public List<ServicoRecorrente> getAll() throws ErroNaQueryException {
+        try {
+            PreparedStatement stm = conexao.prepareStatement("SELECT * FROM t_serv_recorrente");
+            ResultSet result = stm.executeQuery();
+
+            List<ServicoRecorrente> lista = new ArrayList<>();
+            while (result.next()) {
+                Long idServRecorrente = result.getLong("id_serv_recorrente");
+                Long idUsuario = result.getLong("id_usuario");
+                Long idContaBancaria = result.getLong("id_conta_bancaria");
+                String nmServico = result.getString("nm_servico");
+                String descServico = result.getString("desc_servico");
+                Date dtRegistroServ = result.getDate("dt_registro_serv");
+                Date dtLimite = result.getDate("dt_limite");
+                String frequencia = result.getString("frequencia");
+                double valorOperacao = result.getDouble("valor_operacao");
+                char operacaoEfetuada = result.getString("operacao_efetuada").charAt(0);
+                char aPagar = result.getString("a_pagar").charAt(0);
+                char aReceber = result.getString("a_receber").charAt(0);
+
+                lista.add(
+                        new ServicoRecorrente(idServRecorrente, idUsuario, idContaBancaria, nmServico, descServico,
+                        dtRegistroServ, dtLimite, frequencia, valorOperacao, operacaoEfetuada, aPagar, aReceber)
+                );
+            }
+            return lista;
+
+        } catch (SQLException e) {
+            throw new ErroNaQueryException(e.getMessage());
+        }
+    }
+
+    public void fecharConexao() throws NaoFoiPossivelConectarException {
+        try {
+            conexao.close();
+        } catch (SQLException e) {
+            throw new NaoFoiPossivelConectarException(e.getErrorCode());
+        }
+    }
+
 }
