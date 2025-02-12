959a876a07b3c87742929ee7be8c30e26656bb20
Caio Bianchi <caiohbs23@gmail.com>
2025-02-12 18:32:51 -0300

    adicionado projeto comentarios_e_variaveis

---
+++ terceiro_semestre/fase1/cap4/comentarios_e_variaveis/src/main/kotlin/Main.kt
@@ -0,0 +1,23 @@
+fun main(args: Array<String>) {
+
+    // Esse é um comentário Kotlin
+
+    /*
+    Esse é um bloco de texto comentado
+    Esse é um bloco de texto comentado
+    Esse é um bloco de texto comentado
+     */
+
+    var nomeFaculdade = "FIAP"
+    println(nomeFaculdade)
+
+    nomeFaculdade = "Faculdade de Informática e Administração Paulista"
+    println(nomeFaculdade)
+
+    var variavelExplicita: Byte = 19
+    println(variavelExplicita)
+
+    val constante = 3.14
+    println(constante)
+
+}
