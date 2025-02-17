3fa882868160df8da3d05a0539366029fb3d9883
Caio Bianchi <caiohbs23@gmail.com>
2025-02-17 18:45:52 -0300

    projeto finalizado

--- terceiro_semestre/fase1/cap5/BasicComponents/app/src/main/java/com/caiohbs/basiccomponents/MainActivity.kt
+++ terceiro_semestre/fase1/cap5/BasicComponents/app/src/main/java/com/caiohbs/basiccomponents/MainActivity.kt
@@ -6,21 +6,36 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.caiohbs.basiccomponents.ui.theme.BasicComponentsTheme
 
@@ -40,9 +55,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BasicComponentsScreen() {
-    Column(modifier = Modifier
-        .fillMaxWidth()
-        .background(Color.Black)) {
+
+    var textFieldValue = remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Black)
+    ) {
         Text(
             text = "FIAP",
             fontSize = 32.sp,
@@ -62,10 +84,91 @@ fun BasicComponentsScreen() {
             modifier = Modifier.align(Alignment.CenterHorizontally)
         )
         TextField(
-            value = "Android",
-            onValueChange = {},
-            modifier = Modifier.fillMaxWidth()
+            value = textFieldValue.value,
+            onValueChange = { novoValor ->
+                textFieldValue.value = novoValor
+            },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.Words
+            ),
+            label = {
+                Text(text = "Nome e sobrenome")
+            },
         )
+        var quantidade = remember {
+            mutableStateOf("")
+        }
+        TextField(
+            value = "${quantidade.value}",
+            onValueChange = { novoValor ->
+                quantidade.value = novoValor
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            placeholder = {
+                Text(text = "Qual a quantidade?")
+            },
+            colors = TextFieldDefaults.colors(
+                focusedTextColor = Color.White,
+                unfocusedTextColor = Color.Green,
+                unfocusedPlaceholderColor = Color.Magenta
+            )
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Checkbox(
+                checked = false,
+                onCheckedChange = {},
+                colors = CheckboxDefaults.colors(
+                    checkedColor = Color.White,
+                    uncheckedColor = Color(0xffed145b)
+                )
+            )
+            Text(
+                text = "Kotlin",
+                color = Color.White
+            )
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Checkbox(
+                checked = false,
+                onCheckedChange = {},
+                colors = CheckboxDefaults.colors(
+                    checkedColor = Color.White,
+                    uncheckedColor = Color(0xffed145b)
+                )
+            )
+            Text(
+                text = "Java",
+                color = Color.White
+            )
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Checkbox(
+                checked = false,
+                onCheckedChange = {},
+                colors = CheckboxDefaults.colors(
+                    checkedColor = Color.White,
+                    uncheckedColor = Color(0xffed145b)
+                )
+            )
+            Text(
+                text = "C#",
+                color = Color.White
+            )
+        }
     }
 }
 
