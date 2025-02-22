42e22270125a8885f0a457ce2fef2ca9318d679b
Caio Bianchi <caiohbs23@gmail.com>
2025-02-22 11:35:32 -0300

    adicionado novo projeto

---
+++ terceiro_semestre/fase1/cap7/Navegando/app/src/main/java/com/caiohbs/navegando/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.caiohbs.navegando
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Surface
+import com.caiohbs.navegando.ui.theme.NavegandoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NavegandoTheme {
+                Surface() {
+
+                }
+            }
+        }
+    }
+}
