c43f35a27b8c40519434319d9fa2c29244681c0b
Caio Bianchi <caiohbs23@gmail.com>
2025-03-07 18:05:36 -0300

    novo card implementado

--- terceiro_semestre/fase1/cap7/ListasLazy/app/src/main/java/com/caiohbs/listaslazy/MainActivity.kt
+++ terceiro_semestre/fase1/cap7/ListasLazy/app/src/main/java/com/caiohbs/listaslazy/MainActivity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -19,10 +24,14 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.caiohbs.listaslazy.model.Game
+import com.caiohbs.listaslazy.repository.getAllGames
 import com.caiohbs.listaslazy.ui.theme.ListasLazyTheme
 
 class MainActivity : ComponentActivity() {
@@ -68,5 +77,44 @@ fun GamesScreen() {
             }
         )
         Spacer(modifier = Modifier.height(16.dp))
+        LazyColumn() {
+            items(getAllGames()) {
+                GameCard(game = it)
+            }
+        }
+    }
+}
+
+@Composable
+fun GameCard(game: Game) {
+    Card(modifier = Modifier.padding(bottom = 8.dp)) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp).weight(3f)) {
+                Text(
+                    text = game.title,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = game.studio,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Normal
+                )
+            }
+            Text(
+                text = game.releaseYear.toString(),
+                modifier = Modifier.weight(1f).fillMaxWidth(),
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Blue
+            )
+        }
     }
 }
